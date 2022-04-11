15a1d5a33aa78cb249a7a9964f9fc3df5d41ec74
aabertente <abrtent1997@gmail.com>
2022-04-11 07:07:29 +0000

    edit of list problem, try!

--- web_scraping-emails-phones/email_phone_s.py
+++ web_scraping-emails-phones/email_phone_s.py
@@ -1,5 +1,6 @@
 from email.headerregistry import Address
 from itertools import zip_longest
+from multiprocessing import Condition
 from unittest import result
 from bs4 import BeautifulSoup
 import requests
@@ -7,7 +8,18 @@ import csv
 import re
 
 allsites = ["https://estem.ma/",
-            "https://estem.ma/"]
+            "http://www.aui.ma/en/",
+            "http://www.elbiliasup.ma/",
+            "https://emcgi.ma/",
+            "https://www.emsi.ma/",
+            "http://www.esjc.ma/",
+            "https://ecoleheec.ma/",
+            "https://istl.ma/",
+            "https://ueuromed.org/",
+            "https://hem.ac.ma/",
+            "https://www.eigsica.ma/",
+            "https://www.esca.ma/"]
+            #"https://www.uir.ac.ma/"
 emails = []
 phones = []
 for l in allsites:
@@ -15,16 +27,15 @@ for l in allsites:
     soup = BeautifulSoup(result.content, "lxml")
     for link in soup.find_all('a', attrs={'href': re.compile("^mailto:")}):
         emails.append(link.get('href'))
-    for i in range(1):
-        for tele in soup.find_all('a', attrs={'href': re.compile("^tel:")}):
-            phones.append(tele.get('href'))
-        phones.append('\n')
+    for tele in soup.find_all('a', attrs={'href': re.compile("^tel:")}):
+        phones.append(tele.get('href'))
+    #emails.append('\n') # !
 print(emails)
 print(phones)
 
 file_list = [emails, phones]
 exported = zip_longest(*file_list)
-with open("/home/abdellahpc/Bureau/Python/Scraping/data.csv", "w") as data_file:
+with open("/home/abdellahpc/Bureau/Python/Scraping/web_scraping-emails-phones/mydata.csv", "w") as data_file:
     mywriter = csv.writer(data_file)
     mywriter.writerow(["Emails", "Phones"])
     mywriter.writerows(exported)
